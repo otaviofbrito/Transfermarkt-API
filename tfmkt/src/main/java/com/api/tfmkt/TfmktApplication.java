ee124575786af132cb7d62e2545544a0a65908c2
Otavio <otaviofbrito@gmail.com>
2024-07-11 22:26:53 -0300

    Initializr

---
+++ tfmkt/src/main/java/com/api/tfmkt/TfmktApplication.java
@@ -0,0 +1,13 @@
+package com.api.tfmkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfmktApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfmktApplication.class, args);
+    }
+
+}
