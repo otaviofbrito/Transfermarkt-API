c9a6a331d806ac8c2fc35c7947046f6f5772d0ea
Otavio F <otaviofbrito@gmail.com>
2024-08-03 14:14:26 -0300

    Add exception handler for exceptions thrown

--- src/main/java/com/api/tfmkt/exception/FetchTransferException.java
+++
@@ -1,7 +0,0 @@
-package com.api.tfmkt.exception;
-
-public class FetchTransferException extends RuntimeException {
-    public FetchTransferException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/api/tfmkt/exception/GlobalExceptionHandler.java
+++ src/main/java/com/api/tfmkt/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.api.tfmkt.exception;
 import com.api.tfmkt.exception.ErrorResponse.CustomErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -11,12 +10,42 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(NoTransferFoundException.class)
-    public ResponseEntity<CustomErrorResponse> handleNoTransferFoundException(NoTransferFoundException ex) {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<CustomErrorResponse> handleRuntimeException(RuntimeException ex) {
+        CustomErrorResponse errorResponse = new CustomErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(TransferNotFoundException.class)
+    public ResponseEntity<CustomErrorResponse> handleTransferNotFoundException(TransferNotFoundException ex) {
         CustomErrorResponse errorResponse = new CustomErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(PlayerNotFoundException.class)
+    public ResponseEntity<CustomErrorResponse> handlePlayerNotFound(PlayerNotFoundException ex) {
+        CustomErrorResponse errorResponse = new CustomErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ClubNotFoundException.class)
+    public ResponseEntity<CustomErrorResponse> handleClubNotFound(ClubNotFoundException ex) {
+        CustomErrorResponse errorResponse = new CustomErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(LeagueNotFoundException.class)
+    public ResponseEntity<CustomErrorResponse> handleLeagueNotFound(LeagueNotFoundException ex) {
+        CustomErrorResponse errorResponse = new CustomErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ClubLeagueNotFoundException.class)
+    public ResponseEntity<CustomErrorResponse> handleClubLeagueNotFound(ClubLeagueNotFoundException ex) {
+        CustomErrorResponse errorResponse = new CustomErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
 
 
 }
