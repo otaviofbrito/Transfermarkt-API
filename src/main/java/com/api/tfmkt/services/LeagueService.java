e02bafc04f041f9ef3d09d106596717a65b2396d
Otavio F <otaviofbrito@gmail.com>
2024-08-05 00:08:11 -0300

    Handle null arguments

--- src/main/java/com/api/tfmkt/services/LeagueService.java
+++ src/main/java/com/api/tfmkt/services/LeagueService.java
@@ -8,28 +8,38 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class LeagueService {
 
     private LeagueRepository leagueRepository;
+
     @Autowired
     public LeagueService(LeagueRepository leagueRepository) {
         this.leagueRepository = leagueRepository;
     }
 
     public Optional<League> getLeague(String leagueId) {
+        if (Objects.isNull(leagueId)) {
+            throw new IllegalArgumentException("League id cannot be null");
+        }
+
         Optional<League> optionalLeague = leagueRepository.findById(leagueId);
-        if (optionalLeague.isEmpty()){
+        if (optionalLeague.isEmpty()) {
             throw new LeagueNotFoundException("League with id " + leagueId + " not found!");
         }
         return optionalLeague;
     }
 
     public Page<League> getLeagueByName(Pageable pageable, String name) {
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException("League name cannot be null");
+        }
+
         Page<League> page = leagueRepository.findByNameContaining(pageable, name);
-        if (page.isEmpty()){
+        if (page.isEmpty()) {
             throw new LeagueNotFoundException("League with name " + name + " not found!");
         }
         return page;
