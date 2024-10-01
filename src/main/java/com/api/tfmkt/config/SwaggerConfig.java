31bdada5fea0e192fe5b5591d2e88deb82a6c60f
Otavio F <otaviofbrito@gmail.com>
2024-09-30 22:07:35 -0300

    swagger host

--- src/main/java/com/api/tfmkt/config/SwaggerConfig.java
+++ src/main/java/com/api/tfmkt/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.api.tfmkt.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,8 @@ public class SwaggerConfig {
 
     @Value("${app.version}")
     private String appVersion;
+    @Value("${app.host}")
+    private String host;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -23,6 +26,7 @@ public class SwaggerConfig {
                         .description("This API provides endpoints to access data scraped from Transfermarkt website")
                         .contact(new Contact()
                                 .name("Transfermarkt Scraper API")
-                                .url("https://github.com/otaviofbrito/Transfermarkt-API")));
+                                .url("https://github.com/otaviofbrito/Transfermarkt-API")))
+                .addServersItem(new Server().url(host));
     }
 }
